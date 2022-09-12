3de38b60d3d285a5c7de3a6ad0299cf4efad4044
erikleemet <erik.leemet@gmail.com>
2022-09-12 09:45:24 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const alphabet = [
+    "A",
+    "B",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "R",
+    "S",
+    "Š",
+    "Z",
+    "Ž",
+    "T",
+    "U",
+    "V",
+    "Õ",
+    "Ä",
+    "Ö",
+    "Ü",
+  ];
+const secretWordDiv = document.getElementById("secret-word");
+const wrongLetterDiv = document.getElementById("wrong-word");
+const livesDiv = document.getElementById("lives");
+let inCorrectLetter = [];
+let lives = 9;
+let secretWord = '';
+
+fetch('words_hangman.txt')
+.then(response => response.text())
+.then(data => {
+    words = data.split('\n');
+
+    secretWord = words[Math.floor(Math.random() * words.length)];
+
+    initGameBoard();
+
+});
+
+
+function initGameBoard() {
+    for (let i = 0; i < secretWord.length; i++) {
+        const charSpan = document.createElement('span');
+        charSpan.setAttribute('id', 'char-' + i);
+        charSpan.innerText = "_";
+        secretWordDiv.appendChild(charSpan);
+    };
+}
+
+document.addEventListener('keydown', (e) => {
+    const charGuess = e.key.toUpperCase();
+    if (alphabet.includes(charGuess) && lives != 0) {
+
+        let isCorrectLetter = false;
+
+        for (let i = 0; i < secretWord.length; i++) {
+            if (secretWord[i].toUpperCase() == charGuess) {
+                const charSpan = document.getElementById('char-' + i)
+                charSpan.innerText = charGuess.toUpperCase();
+                isCorrectLetter = true;
+            }
+        }
+        if (!isCorrectLetter && !inCorrectLetter.includes(charGuess)) {
+            inCorrectLetter.push(charGuess);
+            wrongLetterDiv.innerHTML += charGuess;
+            lives--;
+            if (lives == 0) {
+                livesDiv.innerHTML = 'game over!<br>' + secretWord;
+            } else {
+                livesDiv.innerText = lives;
+            }
+        }
+    }
+});
